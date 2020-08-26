16c6adc0c50cc844c5d7d972067f638add31cf7f
Daniel <daniel.n96@t-online.de>
2020-08-26 23:15:42 +0200

    refactor shadowRoot

--- addressComponent.js
+++ addressComponent.js
@@ -1,11 +1,19 @@
+const template = document.createElement("template");
+template.innerHTML = `
+<style>
+    div{
+        color: #ccc;
+        font-weight: bold;
+    }
+</style>
+<div> Adresse</div>
+`;
+
 class AddressComponent extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
-    this.shadowRoot.innerHTML = `<style>
-                                    div{color: #ccc;font-weight: bold;}
-                                </style>
-                                <div> Adresse</div>`;
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
   }
 }
 
