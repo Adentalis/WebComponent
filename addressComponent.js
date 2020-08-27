6ebaa0f2f1a1e10ee0d1cf95773310547cd7d2dd
Daniel <daniel.n96@t-online.de>
2020-08-27 13:53:12 +0200

    add basic css grid

--- addressComponent.js
+++ addressComponent.js
@@ -1,48 +1,87 @@
 const template = document.createElement("template");
 template.innerHTML = `
 <style>
-    .component {
-      display: grid;
-      grid-gap: 10px;
-      background-color: #eee;
-      margin: 10px;
-      padding:10px;
-    }
+.component {
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: 70px repeat(5, 1fr);
+  grid-template-rows: repeat(4, 1fr);
+  background-color: #eee;
+  margin: 10px;
+  padding: 10px;
+}
 
-    .component a {
-      color: #555;
-    }
+.component a {
+  color: #555;
+}
 
-    #address {
-      color: #333;
-      font-weight: bold;
-      font-size: 20px;
-    }
-    
+#address {
+  color: #333;
+  font-weight: bold;
+  font-size: 20px;
+  grid-row: 1/2;
+  grid-column: 1/2;
+}
+
+#plz_Label {
+  grid-column: 1/2;
+  grid-row: 2/3;
+}
+#plz_Input {
+  grid-column: 2/3;
+  grid-row: 2/3;
+}
+#city_Label {
+  grid-column: 3/4;
+  grid-row: 2/3;
+}
+#city_Input {
+  grid-column: 4/7;
+  grid-row: 2/3;
+}
+
+#street_Label {
+  grid-column: 1/2;
+  grid-row: 3/4;
+}
+#streets_Select {
+  grid-column: 2/5;
+  grid-row: 3/4;
+}
+#nr_Label {
+  grid-column: 5/6;
+  grid-row: 3/4;
+}
+#nr_Input {
+  grid-column: 6/7;
+  grid-row: 3/4;
+}
+
+#country_Label {
+  grid-column: 1/2;
+  grid-row: 4/5;
+}
+
+#country_Input {
+  grid-column: 2/7;
+  grid-row: 4/5;
+}
 </style>
 <div class="component">
-<div id="address">
-  <a>Adresse</a>
-</div>
+<a id="address">Adresse</a>
 
-<div id="city">
-  <a id="plz_Label">PLZ</a>
-  <input id="plz_Input" type="number" />
-  <a id="city_Label">Stadt</a>
-  <input id="city_Input" />
-</div>
+<a id="plz_Label">PLZ</a>
+<input id="plz_Input" type="number" />
+<a id="city_Label">Stadt</a>
+<input id="city_Input" />
 
-<div id="street">
-  <a id="street_Label">Straße</a>
-  <select id="streets_Select"></select>
-  <a id="nr_Label">Hausnummer</a>
-  <input id="nr_Input" type="number" />
-</div>
+<a id="street_Label">Straße</a>
+<select id="streets_Select"></select>
+<a id="nr_Label">Hausnummer</a>
+<input id="nr_Input" type="number" />
 
-<div id="country">
-  <a id="country_Label">Land</a>
-  <input id="country_Input" />
-</div>
+<a id="country_Label">Land</a>
+<input id="country_Input" />
 </div>
 
 `;
